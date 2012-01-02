f1ebcc7c3ea9300ef6927dd28e5eac678b13b67d
Johannes <schmittjoh@gmail.com>
2012-01-02 14:08:18 +0100

    adds some php doc

--- src/Metadata/MetadataFactoryInterface.php
+++ src/Metadata/MetadataFactoryInterface.php
@@ -18,7 +18,23 @@
 
 namespace Metadata;
 
+/**
+ * Interface for Metadata Factory implementations.
+ *
+ * @author Johannes M. Schmitt <schmittjoh@gmail.com>
+ */
 interface MetadataFactoryInterface
 {
+    /**
+     * Returns the gathered metadata for the given class name.
+     *
+     * If the drivers return instances of MergeableClassMetadata, these will be
+     * merged prior to returning. Otherwise, all metadata for the inheritance 
+     * hierarchy will be returned as ClassHierarchyMetadata unmerged.
+     *
+     * If no metadata is available, null is returned.
+     *
+     * @return ClassHierarchyMetadata|MergeableClassMetadata|null
+     */
     function getMetadataForClass($className);
 }
