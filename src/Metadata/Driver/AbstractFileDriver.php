33abfa8c62ccbe323ca349f66e178dc927456ae7
Martin Hasoň <martin.hason@gmail.com>
2017-07-21 10:02:23 +0200

    Fix an exception message

--- src/Metadata/Driver/AbstractFileDriver.php
+++ src/Metadata/Driver/AbstractFileDriver.php
@@ -34,7 +34,7 @@ abstract class AbstractFileDriver implements AdvancedDriverInterface
     public function getAllClassNames()
     {
         if (!$this->locator instanceof AdvancedFileLocatorInterface) {
-            throw new \RuntimeException('Locator "%s" must be an instance of "AdvancedFileLocatorInterface".');
+            throw new \RuntimeException(sprintf('Locator "%s" must be an instance of "AdvancedFileLocatorInterface".', get_class($this->locator)));
         }
 
         return $this->locator->findAllClasses($this->getExtension());
