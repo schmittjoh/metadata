6af9152795e8d99bc928a4b24fd7ef90727bd1b2
Bob van de Vijver <bobvandevijver@hotmail.com>
2020-12-17 09:56:53 +0100

    Add tests for non-writable error using vfsStream

--- tests/Cache/FileCacheTest.php
+++ tests/Cache/FileCacheTest.php
@@ -7,16 +7,24 @@ namespace Metadata\Tests\Cache;
 use Metadata\Cache\FileCache;
 use Metadata\ClassMetadata;
 use Metadata\Tests\Fixtures\TestObject;
+use org\bovigo\vfs\vfsStream;
 use PHPUnit\Framework\TestCase;
 
 class FileCacheTest extends TestCase
 {
     private $dir;
+    private $nestedDir;
 
     protected function setUp(): void
     {
         $this->dir = sys_get_temp_dir() . '/jms-' . md5(__CLASS__);
+        $this->nestedDir = $this->dir . '/some-dir';
         if (is_dir($this->dir)) {
+            if (is_dir($this->nestedDir)) {
+                array_map('unlink', glob($this->nestedDir . '/*'));
+                rmdir($this->nestedDir);
+            }
+
             array_map('unlink', glob($this->dir . '/*'));
         } else {
             mkdir($this->dir);
@@ -68,5 +76,34 @@ class FileCacheTest extends TestCase
         file_put_contents($this->dir . '/Metadata-Tests-Fixtures-TestObject.cache.php', $fileContents);
 
         $this->assertNull($cache->load(TestObject::class));
+    }
+
+    public function testPutCacheFileInNotExistingDirectory()
+    {
+        $cache = new FileCache($this->nestedDir);
+
+        $reflectionClass = new \ReflectionClass('Metadata\Tests\Fixtures\TestObject');
+        $cache->put($metadata = new ClassMetadata('Metadata\Tests\Fixtures\TestObject'));
+        $this->assertEquals($metadata, $cache->load($reflectionClass->name));
+    }
+
+    public function testThrowExceptionIfCacheDirNotWritable()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Can\'t create directory for cache at "vfs://root/JMS"');
+
+        $root = vfsStream::setup('root', 0555);
+        $cache = new FileCache($root->url() . '/JMS');
+    }
+
+    public function testThrowExceptionIfCacheFilePathNotWritable()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('The directory "vfs://root" is not writable.');
+
+        $root = vfsStream::setup('root', 0555);
+        $cache = new FileCache($root->url());
+
+        $cache->put($metadata = new ClassMetadata('Metadata\Tests\Fixtures\TestParent'));
     }
 }
